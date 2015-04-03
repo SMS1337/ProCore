a53ea7a374f1ca0f3bbbf50d9fcff899351ebc5f
Dylan <nerdense@gmail.com>
2015-04-03 00:11:37 -0400

    Cleaned up! A lot!

--- Chat-Module/MainServer.lua
+++ Chat-Module/MainServer.lua
@@ -7,9 +7,9 @@
 	Need to do: Clean this fucking mess up. Ugly coding, I know lol.
 --]]
 
---Enables the remoteevent server
-
+-- This is an API (correct term?) to prompt moderation shit.
 function promptModeration(player)
+
 	-- Connect compatible moderation systems here for gold text
 	
 	--[[ EXAMPLE:
@@ -17,9 +17,20 @@ function promptModeration(player)
 		return true
 	end
 	]]--
+
 end
 
 
+--To fix my ugly color3 statements
+function rgb(r,g,b)
+	return Color3.new(r/165,g/165,b/165)
+end
+
+SizeX=16
+
+----------------------------------------------------------------------------------------------------------- ACTUAL SCRIPT
+
+-- This creates the RemoteEvent in workspace so users can locally trigger it.
 function bootRemote()
 	script.Parent=game.ServerScriptService
 	if not workspace:findFirstChild'ProChat' then
@@ -34,45 +45,42 @@ function bootRemote()
 	end
 end
 
---To fix my ugly color3 statements
-function rgb(r,g,b)
-	return Color3.new(r/165,g/165,b/165)
-end
-
 --Loops through each player and moves them up + adds the newest message.
 function deliver(instanc)
+	local triggerdel=-117 -- This is to delete the ones after a certain point.
 	for _,a in pairs(game.Players:GetChildren()) do
 		if a.PlayerGui:findFirstChild'chatGui' then
 			for _,b in pairs(a.PlayerGui.chatGui.Frame:GetChildren()) do
 				if b:IsA'Frame' and b.Name=='message' then
-					b:TweenPosition(UDim2.new(0,0,1,b.Position.Y.Offset - 17),"Out","Linear",.1,true)
-					if b.Position.Y.Offset < -117 then -- Most likely going to scale this in the future. 
+					b:TweenPosition(UDim2.new(0,0,1,b.Position.Y.Offset - sizeX-(sizeX*2)-1),"Out","Linear",.1,true)
+					if b.Position.Y.Offset < triggerdel then -- Most likely going to scale this in the future. 
 						b:Destroy()
 					end
 				end
 			end
 			local newMsg=instanc:Clone()
 			newMsg.Parent=a.PlayerGui.chatGui.Frame
-				newMsg.Position=UDim2.new(-1,0,1,-16)--To make sure it animates correctly
-				newMsg:TweenPosition(UDim2.new(0,0,1,-16),"Out","Linear",.1,true)
+				newMsg.Position=UDim2.new(-1,0,1,sizeX-(sizeX*2))--To make sure it animates correctly
+				newMsg:TweenPosition(UDim2.new(0,0,1,sizeX-(sizeX*2)),"Out","Linear",.1,true)
 		end
 	end
 end
 
+-- Does the calculations to generate the messageholder.
 function generateMessage(msg,player)
 	--Create the frame to hold the name + message.
 	local newFrame=Instance.new'Frame'
-		newFrame.Size=UDim2.new(1,0,0,16)
+		newFrame.Size=UDim2.new(1,0,0,sizeX)
 		newFrame.Transparency=0.5
 		newFrame.BackgroundColor3=player.TeamColor.Color
-		newFrame.Position=UDim2.new(0,0,.9,-16)
-		newFrame.Name="message"
+		newFrame.Position=UDim2.new(0,0,.9,-sizeX)
+		newFrame.Name="message" -- We'll use this 
 		newFrame.BorderSizePixel=0
 		
 	--Create the name
 	local nameLabel=Instance.new'TextLabel'
 	
-	--Ths following "if" statement will detect if the player is using nicknames
+	--Ths following "if" statement will detect if the player is using nicknames. Long "if" statement warning.
 	if player:findFirstChild'.nickname' and player[".nickname"]:IsA'StringValue' and player[".nickname"].Value~=string.rep(" ",string.len(player[".nickname"].Value)) and string.len(player[".nickname"].Value)<21 then
 			nameLabel.Text=" ~"..player[".nickname"].Value.." "
 	else
@@ -95,7 +103,6 @@ function generateMessage(msg,player)
 		
 		--The textlabel to hold the message
 	local textLabel=Instance.new'TextLabel'
-		
 		textLabel.Text=" "..msg.." "
 		textLabel.Size=UDim2.new(0,string.len(textLabel.Text)*9,1,0)
 		textLabel.TextColor3=BrickColor.White().Color 
@@ -111,13 +118,9 @@ function generateMessage(msg,player)
 		textLabel.ZIndex=2
 		
 	-- Set the size at the end.
-	newFrame.Size=UDim2.new(
-		0,
-		nameLabel.Size.X.Offset + textLabel.Size.X.Offset,
-		0,
-		16
-	)
-		-- If the player is connected as a moderator it will make their text gold.
+	newFrame.Size=UDim2.new(0,nameLabel.Size.X.Offset + textLabel.Size.X.Offset,0,16)
+
+	-- If the player is connected as a moderator it will make their text gold. I should really improve this lmao.
 	if promptModeration(player) then
 		textLabel.TextColor3=rgb(241, 196, 15)
 	end
@@ -126,9 +129,10 @@ function generateMessage(msg,player)
 	deliver(newFrame)
 end
 
+-- This will continue the function bootRemote until it is launched.
 repeat bootRemote() wait() print'Booting Chat events...' until workspace:findFirstChild'ProChat'
 
---onserverevent
+-- Calculate OnServerEvent
 workspace.ProChat.Chatted2.OnServerEvent:connect(function(player,msg)
 	if player and msg~=nil then
 		generateMessage(msg,player)
