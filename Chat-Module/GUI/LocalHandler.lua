dd7743b18f05ef5060219afff657a2213520f20a
Dylan <nerdense@gmail.com>
2015-04-02 23:23:47 -0400

    Create LocalHandler.lua

---
+++ Chat-Module/GUI/LocalHandler.lua
@@ -0,0 +1,64 @@
+-- I WARN YOU. THIS SCRIPT IS GROSS.
+local player = game.Players.LocalPlayer
+	local mouse = player:GetMouse()
+typer = script.Parent.input
+maxnumber=200
+stockphrase = " Tap here or press '/' to begin typing."
+UID = game:GetService("UserInputService")
+
+--Remove CoreGUI. 
+local StarterGui = game:GetService('StarterGui')
+StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+	
+--Captures focus	
+function animate()
+	typer.Text = ""
+	typer:CaptureFocus()
+end
+
+--Catches the "/" keydown
+mouse.KeyUp:connect(function(key)
+	if key=="/" then
+		animate()
+	end
+end)
+
+--workspace["Chat Servers"].ChatStrapper:FireServer(msg)
+UID.InputBegan:connect(function(input)
+	if input.KeyCode == Enum.KeyCode.Return then
+		if typer.Text ~= stockphrase and typer.Text ~= "" and typer.Text ~=" " then
+			--Good good, let's continue.
+			if workspace:findFirstChild'ProChat' then
+				workspace.ProChat.Chatted2:FireServer(string.sub(typer.Text,0,maxnumber))
+				typer.Text = stockphrase
+			end		
+		end
+	end
+end)
+
+--This is to track the character count of the textbox, ugly because I can't find a proper event.
+while wait() do
+	
+	local number = string.len(script.Parent.input.Text)
+	
+	--Fill the label
+	if typer.Text ~= stockphrase and typer.Text ~= "" then
+		script.Parent.char.Text = maxnumber-number.." characters left"
+	else
+		number=0
+		script.Parent.char.Text = maxnumber-number.." characters left"
+	end
+	
+	--Set the color
+	if number>(maxnumber-(maxnumber/3)) then
+		script.Parent.char.TextColor3=Color3.new(212/255, 19/255, 22/255)
+		else
+		script.Parent.char.TextColor3=Color3.new(10/255, 151/255, 212/255)
+	end
+	
+	--Set it back to the correct length.
+	if number>maxnumber then
+		typer.Text = string.sub(typer.Text,0,maxnumber)
+	end 
+	
+end
