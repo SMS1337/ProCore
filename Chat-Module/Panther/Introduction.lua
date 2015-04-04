ee3772a51f9a0c5c5def0d3e5363f2120354fdc3
Dylan <imeprent@gmail.com>
2015-04-04 17:08:29 -0400

    Create Introduction.lua

---
+++ Chat-Module/Panther/Introduction.lua
@@ -0,0 +1,28 @@
+--[[
+	This is a folder of neccesary files to get ProChat working in your server.
+	Don't modify it.
+	
+	View the GitHub here:
+	https://github.com/eprent/ProCore/
+	This has (if needed) weekly updates, GitHub updates are bleeding edge.
+	If you have an issue, report it on GitHub please.
+	
+	-----------------------------------------------------------------
+	|															                                 	|
+	|					/!\ LICENSE AND AGREEMENT				                    	|
+	|		-Only re-release this if you have made changes.          		|
+	|		-By editing scripts we won't help fix your issues.	      	|
+	|														                                		|
+	|													                                 			|
+	|					  This is FREE software.			                    		|
+	|			 If you paid for it, get your money back.		            	|
+	|				  And report the seller to ROBLOX.				              |
+	|															                                	|
+	-----------------------------------------------------------------
+	
+	v1.0.0
+	
+	--
+	Contributors: 
+		-eprent
+--]]
