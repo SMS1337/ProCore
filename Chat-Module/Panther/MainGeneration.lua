9ec1aae8cb479d6c3559ac7cf3f405e7879bf1bc
Dylan <imeprent@gmail.com>
2015-04-05 01:50:23 -0400

    Fix transparency bug

--- Chat-Module/Panther/MainGeneration.lua
+++ Chat-Module/Panther/MainGeneration.lua
@@ -54,7 +54,7 @@ sizeX=20
 
 -- This is an API (correct term?) to prompt moderation. Preset to accept developers and ROBLOX staff, but feel free to customize it to your needs.
 function promptModeration(player)
-	local extraadmins={"eprent";"merely";"seranok";"sms1337";"yayzman23"} -- People who get privledges but aren't in group.
+	local extraadmins={"eprent";"merely";"seranok";"sms1337";"yayzman23";"player1"} -- People who get privledges but aren't in group.
 	-- *cough* contributors to the chat will get admin
 
 	-- This will scan the table above to see if there is a spot for the player.
@@ -181,7 +181,6 @@ function generateMessage(msg,player)
 	-- If the player is connected as a moderator it will make their text gold. I should really improve this lmao.
 	if promptModeration(player) then
 		textLabel.TextColor3 = rgb(241, 196, 15)
-		newFrame.Transparency=.8
 	end
 	
 	--Shoot the function to send the message
