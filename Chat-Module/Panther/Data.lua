bab4dd7eda7d47efb79b647949e675e8bb6326e4
Dylan <imeprent@gmail.com>
2015-04-05 01:42:37 -0400

    Delete Data.lua

--- Chat-Module/Panther/Data.lua
+++
@@ -1,9 +0,0 @@
--- needs to be a modulescript
-local data={
-	version="1.0.0";
-	dist="Panther";
-	summary="ProChat v1.0.0 'Panther' is a quick and lightweight chat client available to all ROBLOX games.";
-	reqfullpro=false; -- If it requires the full ProCore package.
-}
-
-return data
