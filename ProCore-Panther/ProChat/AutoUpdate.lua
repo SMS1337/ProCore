fed784e4482a050402eeaf8bfcfb9ca14b02a008
Dylan <imeprent@gmail.com>
2015-04-12 20:05:51 -0400

    Fix nil error

--- ProCore-Panther/ProChat/AutoUpdate.lua
+++ ProCore-Panther/ProChat/AutoUpdate.lua
@@ -3,20 +3,35 @@ local modelid = 234218451
 
 local model = game:GetService("InsertService"):LoadAsset(modelid)
 model = model:GetChildren()[1]
-if model:WaitForChild("Version").Value > version then
-	model:WaitForChild("Settings"):Destroy()
-	local realsettings = script.Parent:WaitForChild("Settings"):Clone()
-	realsettings.Parent = model
-	print("------------------------------------PROCHAT AUTO UPDATER------------------------------------")
-	game:GetService('TestService'):Warn(false, "ProChat just automagically updated to version " .. model:WaitForChild("Version").Value..", courtesy of the ProChat contributers")
-	game:GetService('TestService'):Warn(false, "Don't worry, all your settings should carry over, you should consider manually updating sometime though")	
-	game:GetService('TestService'):Warn(false, "If an update added new settings, they will be set to their default values")	
-	print("------------------------------------PROCHAT AUTO UPDATER------------------------------------")	
-	model.Parent = script.Parent.Parent
-	script.Parent:Destroy()
-else
-	print("did not update")
+
+local function printf(text)
+	game:GetService('TestService'):Warn(false, " [ ProChat Auto Update Function ] "..text)
+end
+
+local function continue()
 	canrun = Instance.new("BoolValue")
 	canrun.Name = "StartExec"
 	canrun.Parent = script.Parent
+end
+
+if model~= nil then -- If it's not trusted, it will nil. 
+	if model:WaitForChild("Version").Value > version then
+		model:WaitForChild("Settings"):Destroy()
+		local realsettings = script.Parent:WaitForChild("Settings"):Clone()
+			realsettings.Parent = model
+
+		printf("ProChat just automagically updated to version " .. model:WaitForChild("Version").Value.. ", courtesy of the ProChat contributers")
+		printf("Don't worry, all your settings should carry over, you should consider manually updating sometime though")
+		printf("If an update added new settings, they will be set to their default values")
+		
+		model.Parent = script.Parent.Parent
+		script.Parent:Destroy()
+	else
+		print("did not update")
+		continue()
+	end
+else -- If the model is nil
+	printf"Auto Update was disabled due to an fatal error."
+	continue()
+	script.Disabled=true
 end
