52437022d8e4163437e0f309df51c74dc2e1a424
SMS1337 <notascrublord@gmail.com>
2015-04-16 18:05:53 -0700

    chat bubbles

--- ProCore-Panther/ProChat/Settings.lua
+++ ProCore-Panther/ProChat/Settings.lua
@@ -1,5 +1,6 @@
 local settings = {}
 	settings["nicknameLocked"] = true -- If set to true, nicknames are only for admins.
 	settings["feedback"] = false --Gives you output spam on everything that happens, good for debugging.
+	settings["chatbubbles"] = true -- Chat bubbles over your head
 	settings["extraadmins"] = {"eprent";"merely";"seranok";"sms1337";"yayzman23"} --people who get admin badge, defaults to contributers
 return settings
